470bb0f376507b5c3308a108100a6c7459f83da1
tiendat0139 <dattien090909@gmail.com>
2023-09-08 12:26:36 +0700

    feat: sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
     get "signup", to: "users#new"
+    get "login", to: "sessions#new"
+    post "login", to: "sessions#create"
+    delete "logout", to: "sessions#destroy"
     resources :users, only: :create
     resources :categories, param: :slug, only: :show
     resources :cuisines, param: :slug, only: %i(index show)
